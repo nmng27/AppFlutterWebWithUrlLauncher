21750b9af76edabd5c135b2b474bbb93b8ebad19
nmng27 <mangussinicholas@gmail.com>
2025-06-20 14:41:13 -0300

    Fim

--- lib/model/url_json.dart
+++ lib/model/url_json.dart
@@ -1,14 +1,14 @@
 List<Map<String, dynamic>> UrlJson = [
   {
-    "url":"https:www.mult-e.com.br",
+    "url":"https://www.mult-e.com.br",
     
   },
   {
-    "url":"https:www.mult-e.com.br",
+    "url":"https://www.uol.com.br/",
     
   },
   {
-    "url":"https:www.mult-e.com.br",
+    "url":"https://www.terra.com.br/",
     
   },
 ];
--- lib/widget/Iframe.dart
+++ lib/widget/Iframe.dart
@@ -1,66 +1,120 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-import 'dart:ui' as ui;
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:app_view/controller/url_controller.dart';
-import 'package:app_view/model/url.dart';
-import 'package:app_view/model/url_json.dart';
+import 'package:app_view/model/url.dart'; // Assuming this is your Url model
+import 'package:app_view/model/url_json.dart'; // Assuming this is your JSON data source
+import 'package:flutter/material.dart'; // Import material.dart for Column/Scaffold
+import 'package:flutter/widgets.dart'; // Already imported, but good practice
+import 'package:get/get.dart';
+import 'dart:html' as html;
 
-class IframeWidget extends StatelessWidget {
-  const IframeWidget({super.key});
+class IframeWidget extends StatefulWidget {
+  const IframeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _IframeWidget();
+}
+
+class _IframeWidget extends State<IframeWidget> {
+  var urls = []; 
+  bool _isLoading = true; 
+
+  @override
+  void initState() {
+    super.initState(); 
+    _loadUrls();
+  }
+
+  void _loadUrls() {
+    
+    UrlController controller = Get.put(UrlController());
+    setState(() {
+      urls = controller.transformJson(UrlJson);
+      _isLoading = false; 
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (!kIsWeb) {
-      return const Center(
-        child: Text("Este widget funciona apenas no Flutter Web."),
+    Size size = MediaQuery.of(context).size;
+
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    // List to hold the generated iframe widgets
+    List<Widget> iframeWidgets = [];
+
+   
+    if (urls.isNotEmpty) {
+      // First iframe (item 0)
+      iframeWidgets.add(
+        SizedBox(
+          width: size.width * 1.0,
+          height: size.height * 0.5, 
+          child: HtmlElementView.fromTagName(
+            tagName: "iframe",
+            onElementCreated: (element) {
+              if (element is html.IFrameElement) {
+                element.src = urls[0].url;
+                element.allowFullscreen = true; 
+                element.style.border = 'none'; 
+              }
+            },
+          ),
+        ),
       );
     }
 
-    final List<Url> urls = UrlController().transformJson(UrlJson);
-
-    return ListView.builder(
-      itemCount: urls.length,
-      itemBuilder: (context, index) {
-        final url = urls[index];
-        final String viewId = 'iframe_$index';
-
-        // ignore: undefined_prefixed_name
-        ui.platformViewRegistry.registerViewFactory(
-          viewId,
-          (int viewId) => html.IFrameElement()
-            ..src = url.url
-            ..style.border = 'none'
-            ..width = '100%'
-            ..height = '100%',
-        );
-
-        
-
-        if (index == 0) {
-          return SizedBox(
-            width: MediaQuery.of(context).size.width*1.0,
-            height: MediaQuery.of(context).size.height*0.5,
-            child: HtmlElementView(viewType: viewId),
-          );
-        } else if(index == 1 || index == 2){
-          return Row(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width *0.5,
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: HtmlElementView(viewType: viewId),
+    
+    if (urls.length >= 3) { 
+      iframeWidgets.add(
+        Row(
+          children: [
+            Expanded( 
+              child: SizedBox(
+                height: size.height * 0.45, // Adjusted height
+                child: HtmlElementView.fromTagName(
+                  tagName: "iframe",
+                  onElementCreated: (element) {
+                    if (element is html.IFrameElement) {
+                      element.src = urls[1].url;
+                      element.allowFullscreen = true;
+                      element.style.border = 'none';
+                    }
+                  },
+                ),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                height: MediaQuery.of(context).size.height * 0.5,
-              )
-            ],
-          );
-        }
-      },
+            ),
+            Expanded( 
+              child: SizedBox(
+                height: size.height * 0.45, 
+                child: HtmlElementView.fromTagName(
+                  tagName: "iframe",
+                  onElementCreated: (element) {
+                    if (element is html.IFrameElement) {
+                      element.src = urls[2].url;
+                      element.allowFullscreen = true;
+                      element.style.border = 'none';
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else if (urls.length == 2) {
+      
+    }
+
+
+    
+    return Column(
+      children: iframeWidgets,
     );
   }
 }
